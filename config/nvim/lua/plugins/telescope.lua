26fe3e2552b11115ce9746f838ec257f26a653b5
Sam <seigen22@gmail.com>
2025-04-17 12:26:18 -0500

    pkg update, change dep on fzf-native

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -6,8 +6,7 @@ return {
 			"nvim-lua/plenary.nvim",
 			{
 				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-				cond = vim.fn.executable("cmake") == 1,
+				build = "make",
 			},
 			"Myzel394/jsonfly.nvim",
 		},
